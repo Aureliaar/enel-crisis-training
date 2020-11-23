87e38f685eff59ac9997188440663a4c45d5997d
bri_C <briblack.95@outlook.com>
2020-11-23 20:04:07 +0100

    script fix

--- web/scripts.js
+++ web/scripts.js
@@ -54,58 +54,63 @@ class Modifier {
   }
 
 
+function initChart(){
+    var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+    var xAxis = ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45']
+    var graphValues = [639, 465, 493, 478, 589, 632, 674]
+    var curveMaxVal = 600
 
-var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
-var xAxis = ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45']
-var graphValues = [639, 465, 493, 478, 589, 632, 674]
-var curveMaxVal = 600
+    var chartData = {
+        labels: xAxis,
+        datasets: [
+        {
+            label: '# Clients disconnected (thousands)',
+            data: graphValues,
+            backgroundColor: 'transparent',
+            borderColor: colors[0],
+            borderWidth: 4,
+            pointBackgroundColor: colors[0]
+        }]
+    };
+    var ctx = document.getElementById('myChart');
 
-var chartData = {
-    labels: xAxis,
-    datasets: [
-    {
-        label: '# Clients disconnected (thousands)',
-        data: graphValues,
-        backgroundColor: 'transparent',
-        borderColor: colors[0],
-        borderWidth: 4,
-        pointBackgroundColor: colors[0]
-    }]
-};
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: chartData,
-    options: {
-        elements: {
-            point:{
-              radius: 0
-            }
-          },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: chartData,
+        options: {
+            elements: {
+                point:{
+                  radius: 0
                 }
-            }]
+              },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
-});
-setInterval(function(){
-    if (graphValues.length == xAxis.length){
-        graphValues.shift(); //removes the first element of the array
-    }
+    });
+    setInterval(function(){
+        if (graphValues.length == xAxis.length){
+            graphValues.shift(); //removes the first element of the array
+        }
+    
+        graphValues.push(Math.floor((Math.random() * curveMaxVal) + 1)) //add elem at the end of the array
+        for (i=0; i<graphValues.length; i++){
+            myChart.data.datasets[0].data[i] = graphValues[i];
+        }
+    
+        myChart.update();
+    }, 600)
+}
 
-    graphValues.push(Math.floor((Math.random() * curveMaxVal) + 1)) //add elem at the end of the array
-    for (i=0; i<graphValues.length; i++){
-        myChart.data.datasets[0].data[i] = graphValues[i];
-    }
 
-    myChart.update();
-}, 600)
 
 function buttonClick(){
     curveMaxVal = curveMaxVal-200;
+    console.log(document.getElementById("button1"));
     clickable = new ClickableModifier( 0, document.getElementById("button1"));
     //document.getElementById("button2").classList.add('Squad');
 }
