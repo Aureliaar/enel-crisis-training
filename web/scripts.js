39bce18a28b0259d9eba8c1d6e58ff74e37b96d6
aureon <jacopo.sinigaglia@gmail.com>
2020-11-24 19:57:54 +0100

    Let me be clear: scoping in javascript is A STUPID, STUPID, STUPID GODDAMN SHITTY MESS.

--- web/scripts.js
+++ web/scripts.js
@@ -1,7 +1,8 @@
 class Modifier {
     constructor(mod) {
-      this.mod = mod;
-      this.time = 0;
+        let that = this;
+        this.mod = mod;
+        this.time = 0;
     }
     update(delta_time){
         this.time = this.time + delta_time;
@@ -14,23 +15,26 @@ class Modifier {
     
     constructor(mod, buttonref) {
         super(mod);
+        let that = this;
+
         this.mod = mod;
         this.buttonref = buttonref
         this.time = 0;
         this.clicked = false;
-        buttonref.onclick = this.click;
-        console.log(this+" init");
+        buttonref.addEventListener('click', ()=>{ 
+            this.click();
+        })
         
-        console.log(this.buttonref.id);
     }
-    isClickable() {return true;}
     click(){
-        console.log(this);
+        console.log("click event");
 
-        if(this.isClickable){
+        //if(this.isClickable){
             this.clicked = true;
-        }
+        //}
     };
+    isClickable() {return true;}
+
     
     
   }
@@ -42,24 +46,29 @@ class Modifier {
         this.maxCooldown = 120;
         this.maxDuration = 60;
         this.modpersec = -1;
+        setInterval(() => {
+            this.update(0.066);
+        }, 66)
       }
     update(delta_time){
         if (!this.clicked) return;
         this.time += delta_time;
+
         
         if (!this.isInCooldown()){
-            this.mod += this.modpersec;
+            this.mod += this.modpersec * delta_time;
         }
     }
     isInCooldown(){
         let total = this.maxCooldown + this.maxDuration;
-        let t = time%total;
-        if (t<this.maxDuration){
+        let t = this.time%total;
+        if (t<=this.maxDuration){
             return false;
         }        
         return true;
     }
     isClickable(){
+        console.log(this.isInCooldown());
         return this.isInCooldown();
     }
   }
@@ -120,12 +129,12 @@ function initChart(){
 function initButtonsAndChart(){
     initChart();
     initButton("button1");
-    initButton("button2");
+    //initButton("button2");
 }
 
 function initButton(buttonId){
     console.log(document.getElementById(buttonId));
-    clickable = new ClickableModifier( 0, document.getElementById(buttonId));
+    clickable = new Squad( 0, document.getElementById(buttonId));
     console.log(clickable.isClickable());
 }
 
