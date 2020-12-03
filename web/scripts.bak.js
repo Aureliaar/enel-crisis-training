22e1a16b88f9f7f7448ca541f5e1f19cbba80089
aureon <jacopo.sinigaglia@gmail.com>
2020-12-03 18:24:48 +0100

    temp

--- web/scripts.bak.js
+++ web/scripts.bak.js
@@ -8,63 +8,63 @@ class Modifier {
             this.tick()
         }, 330)
     }
-    tick(){
+    tick() {
         let now = Date.now();
-        let delta =  now - this.lastUpdateStamp;
+        let delta = now - this.lastUpdateStamp;
         this.update(delta / 1000);
         this.lastUpdateStamp = now;
     }
-    update(delta_time){
+    update(delta_time) {
         this.time = this.time + delta_time;
 
     }
 
-} 
+}
 
-class SelfStoppingModifier extends Modifier{
+class SelfStoppingModifier extends Modifier {
     constructor(base_mod, mod_per_sec, duration) {
         super(base_mod);
         this.mod_per_sec = mod_per_sec;
         hardcodedModInstances.push(this);
-        
+
         setTimeout(() => {
             clearInterval(this.timer);
 
         }, duration)
     }
-    update(delta_time){
+    update(delta_time) {
         console.log(this.mod_per_sec);
         this.mod += this.mod_per_sec * delta_time;
     }
 }
 
 class ClickableModifier extends Modifier {
-    
-constructor(mod, buttonref) {
-    super(mod);
-    let that = this;
 
-    this.mod = mod;
-    this.buttonref = buttonref
-    this.time = 0;
-    this.clicked = false;
-    buttonref.addEventListener('click', ()=>{ 
-        this.click();
-    })
-        
+    constructor(mod, buttonref) {
+        super(mod);
+        let that = this;
+
+        this.mod = mod;
+        this.buttonref = buttonref
+        this.time = 0;
+        this.clicked = false;
+        buttonref.addEventListener('click', () => {
+            this.click();
+        })
+
     }
-    click(){
+    click() {
         console.log("click event");
 
         //if(this.isClickable){
-            this.clicked = true;
+        this.clicked = true;
         //}
     };
-    isClickable() {return true;}
+    isClickable() { return true; }
 }
 
-var maxSquads = 100; 
-const maxGenerators = 30; 
+var maxSquads = 100;
+const maxGenerators = 30;
 const maxTaskForces = 30;
 var level1Crisis = false;
 var level2Crisis = false;
@@ -102,46 +102,37 @@ class Squad extends ClickableModifier {
         this.status = squadStatus.DEPLOYING;
 
     }
-    update(delta_time){
+    destroy() {
+        globalMod += this.mod;
+        const index = squadInstances.indexOf(this);
+        if (index > -1) {
+            squadInstances.splice(index, 1);
+        }
+        clearInterval(this.timer);
+    }
+
+    update(delta_time) {
         this.time += delta_time;
 
-        if (this.time >= this.activationDelay){
+        if (this.time >= this.activationDelay && this.time < this.activationDelay + this.maxDuration) {
             this.status = squadStatus.DEPLOYED;
         }
-        
-        // if (!this.isInCooldown()){
-        if (this.status == squadStatus.DEPLOYED){
-            this.mod += this.modpersec * delta_time * calcSquadOvercrowdMod();
-        }
-        if (this.time >= (this.activationDelay+this.maxDuration)){
-            globalMod += this.mod;
+        if (this.time >= (this.activationDelay + this.maxDuration)) {
             this.status = squadStatus.RESTING;
         }
-        if (this.time >= (this.activationDelay+this.maxDuration+this.restingDuration) ){
-            
-            const index = squadInstances.indexOf(this);
-            if (index > -1) {
-                squadInstances.splice(index, 1);
-            }
-            clearInterval(this.timer);
+
+        if (this.time >= (this.activationDelay + this.maxDuration + this.restingDuration)) {
+            this.destroy()
+        }
+        if (this.status == squadStatus.DEPLOYED) {
+            this.mod += this.modpersec * delta_time * calcSquadOvercrowdMod();
         }
 
     }
-    isInCooldown(){
-        let total = this.maxCooldown + this.maxDuration;
-        let t = this.time%total;
-        if (t<=this.maxDuration){
-            return false;
-        }        
-        return true;
-    }
-    isClickable(){
-        console.log(this.isInCooldown());
-        return this.isInCooldown();
-    }
+
 }
 
-class GruppoElettrogeno extends ClickableModifier{
+class GruppoElettrogeno extends ClickableModifier {
     constructor(mod, buttonref) {
         super(mod, buttonref);
         this.activationDelay = Math.random() * (60 - 30) + 30;  //Random integer between 30 and 60
@@ -151,21 +142,21 @@ class GruppoElettrogeno extends ClickableModifier{
             this.update(0.066);
         }, 66)
     }
-    update(delta_time){
+    update(delta_time) {
         this.time += delta_time;
 
-        if (this.time >= this.activationDelay){
+        if (this.time >= this.activationDelay) {
             this.status = squadStatus.DEPLOYED;
         }
-        
-        if (this.status == squadStatus.DEPLOYED){
+
+        if (this.status == squadStatus.DEPLOYED) {
             console.log("am doing stuff");
-            this.mod += this.modpersec * delta_time  * calcSquadOvercrowdMod();;
+            this.mod += this.modpersec * delta_time * calcSquadOvercrowdMod();;
         }
     }
 }
 
-class TaskForce extends Squad{
+class TaskForce extends Squad {
     constructor(mod, buttonref) {
         super(mod, buttonref);
         this.activationDelay = 120;
@@ -190,72 +181,72 @@ class Weather extends Modifier {
             case weather.BAD: return 0.3;
             case weather.ACCEPTABLE: return 1;
             case weather.PERFECT: return 1.5;
-    }
+        }
     }
 
 }
 
-function calcModFor(array){
+function calcModFor(array) {
     var tempMod = 0;
-    for (i=0; i<array.length; i++){
-        console.log(tempMod + " - " + array[i].mod) ;
+    for (i = 0; i < array.length; i++) {
+        console.log(tempMod + " - " + array[i].mod);
         tempMod += array[i].mod;
     }
     return tempMod;
 }
-function calcSquadOvercrowdMod(){
+function calcSquadOvercrowdMod() {
     var activeSquadsPerc = squadInstances.filter(squad => squad.status == squadStatus.DEPLOYED).length / maxSquads;
     return 1 - activeSquadsPerc;
 
 }
-function calcTotalMod(){
+function calcTotalMod() {
     var totalMod = 0;
-    var squadMod = calcModFor(squadInstances.filter(squad => squad.status == squadStatus.DEPLOYED));
+    var squadMod = calcModFor(squadInstances);
     var genMod = calcModFor(generatorInstances.filter(generator => generator.status == squadStatus.DEPLOYED));
-    var taskMod = calcModFor(taskForceInstances.filter(taskForce => taskForce.status == squadStatus.DEPLOYED));
+    var taskMod = calcModFor(taskForceInstances);
     var hardcodedMod = calcModFor(hardcodedModInstances);
     return squadMod + genMod + taskMod + hardcodedMod + globalMod;
 }
 
-function initButton(buttonId){
+function initButton(buttonId) {
     console.log(document.getElementById(buttonId));
-    clickable = new Squad( 1000, document.getElementById(buttonId));
+    clickable = new Squad(1000, document.getElementById(buttonId));
     console.log(clickable.isClickable());
 }
 
-function sendSquad(buttonId){
-    clickable = new Squad( 0, document.getElementById(buttonId));
+function sendSquad(buttonId) {
+    clickable = new Squad(0, document.getElementById(buttonId));
     squadInstances.push(clickable);
 }
 
-function sendTaskForce(buttonId){
-    clickable = new TaskForce( 0, document.getElementById(buttonId));
+function sendTaskForce(buttonId) {
+    clickable = new TaskForce(0, document.getElementById(buttonId));
     taskForceInstances.push(clickable);
 }
 
-function sendGenerator(buttonId){
-    clickable = new GruppoElettrogeno( -100, document.getElementById(buttonId));
+function sendGenerator(buttonId) {
+    clickable = new GruppoElettrogeno(-100, document.getElementById(buttonId));
     generatorInstances.push(clickable);
-    maxSquads-=1
+    maxSquads -= 1
 }
 
-function initGruppoElettrogeno(buttonId){
+function initGruppoElettrogeno(buttonId) {
     console.log(document.getElementById(buttonId));
-    clickable = new GruppoElettrogeno( 0, document.getElementById(buttonId));
+    clickable = new GruppoElettrogeno(0, document.getElementById(buttonId));
     console.log(clickable.isClickable());
 }
 
-function initTaskForce(buttonId){
+function initTaskForce(buttonId) {
     console.log(document.getElementById(buttonId));
-    clickable = new TaskForce( 0, document.getElementById(buttonId));
+    clickable = new TaskForce(0, document.getElementById(buttonId));
 }
 
-function declareLvl1Crisis(){
+function declareLvl1Crisis() {
     level1Crisis = true;
     document.getElementById("crisisLevel").innerHTML = "Emergency level 2";
 }
 
-function declareLvl2Crisis(){
+function declareLvl2Crisis() {
     level2Crisis = true;
     document.getElementById("crisisLevel").innerHTML = "Critical Emergency";
 }
