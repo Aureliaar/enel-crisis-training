2c5ed72057c6f8bb7d831b9f4345506982571c78
bri_C <briblack.95@outlook.com>
2020-12-06 12:01:01 +0100

    link generator

--- web/syncStartScript.js
+++ web/syncStartScript.js
@@ -2,18 +2,21 @@
 // test: /index.html?h=11&m=3&s=0
 
 const params = new URLSearchParams(document.location.search);
-const hParam = params.get("h");
-const mParam = params.get("m");
-const sParam = params.get("s");
+const paramUtcDate = params.get("utcDate");
+// const hParam = params.get("h");
+// const mParam = params.get("m");
+// const sParam = params.get("s");
 
 var intervalId = window.setInterval(checkTime, 500);
 
 function checkTime() {
 
     var d = new Date();
-    var h = d.getHours();
-    var m = d.getMinutes();
-    var s = d.getSeconds();
+    var utcDate = Date.UTC(d.getFullYear(), d.getMonth(), d.getDay(), d.getMinutes(), d.getSeconds());
+    // var h = d.getHours();
+    // var m = d.getMinutes();
+    // var s = d.getSeconds();
 
-    if(h == hParam && m == mParam && s == sParam) return window.location='game.html';
+    //if(h == hParam && m == mParam && s == sParam) return window.location='game.html';
+    if(paramUtcDate>= utcDate ){return window.location='game.html';} 
 }
